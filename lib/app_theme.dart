1a15e8e6883b436c02d1ba4720601336a984ba82
ShrkoXIII <a0962028201@gmail.com>
2025-07-16 17:51:14 +0300

    finishing quran tap

--- lib/app_theme.dart
+++ lib/app_theme.dart
@@ -5,20 +5,33 @@ class AppTheme {
   static Color black = Color(0xFF242424);
   static Color wight = Colors.white;
   static ThemeData light_theme = ThemeData(
-      primaryColor: primarylight,
-      scaffoldBackgroundColor: Colors.transparent,
-      appBarTheme: AppBarTheme(
-          backgroundColor: Colors.transparent,
-          centerTitle: true,
-          titleTextStyle: TextStyle(
-            fontSize: 30,
-            fontWeight: FontWeight.bold,
-            color: black,
-          )),
-      bottomNavigationBarTheme: BottomNavigationBarThemeData(
-        backgroundColor: primarylight,
-        selectedItemColor: black,
-        unselectedItemColor: wight,
-      ));
+    primaryColor: primarylight,
+    scaffoldBackgroundColor: Colors.transparent,
+    appBarTheme: AppBarTheme(
+        backgroundColor: Colors.transparent,
+        centerTitle: true,
+        titleTextStyle: TextStyle(
+          fontSize: 30,
+          fontWeight: FontWeight.bold,
+          color: black,
+        )),
+    bottomNavigationBarTheme: BottomNavigationBarThemeData(
+      backgroundColor: primarylight,
+      selectedItemColor: black,
+      unselectedItemColor: wight,
+    ),
+    textTheme: TextTheme(
+      headlineSmall: TextStyle(
+        fontSize: 25,
+        fontWeight: FontWeight.w400,
+        color: black,
+      ),
+      titleLarge: TextStyle(
+        fontSize: 20,
+        fontWeight: FontWeight.w400,
+        color: black,
+      ),
+    ),
+  );
   static ThemeData dark_theme = ThemeData();
 }
